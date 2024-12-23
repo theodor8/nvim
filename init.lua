38e077e79a5aae122c83f8650c2249ee33254fdc
Theodor Djurström <thdj5842@fries.it.uu.se>
2024-12-23 17:01:02 +0100

    {}

--- init.lua
+++ init.lua
@@ -204,6 +204,8 @@ vim.keymap.set({ 'n', 'v' }, 'K', '5k')
 
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '{', '{zz')
+vim.keymap.set('n', '}', '}zz')
 
 vim.keymap.set('i', 'jk', '<Esc>')
 
