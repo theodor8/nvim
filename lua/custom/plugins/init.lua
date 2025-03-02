bb26ebe4c74d3db74162e83871dc6024766d1fbf
Theodor Djurström <theodju@gmail.com>
2025-03-02 12:57:54 +0100

    commented out copilot and pyright

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -22,40 +22,40 @@ return {
   --     },
   --   },
   -- },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    event = "InsertEnter",
-    opts = {
-      suggestion = {
-        auto_trigger = true,
-        keymap = {
-          accept = "<Tab>",
-          next = "<C-j>",
-          prev = "<C-k>",
-        }
-      }
-    },
-    init = function()
-      vim.cmd(":Copilot disable")
-    end,
-    keys = {
-      {
-        "<leader>cps",
-        "<cmd>Copilot status<CR>",
-      },
-      {
-        "<leader>cpe",
-        "<cmd>Copilot enable<CR>",
-      },
-      {
-        "<leader>cpd",
-        "<cmd>Copilot disable<CR>",
-      },
-    },
-
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   build = ":Copilot auth",
+  --   event = "InsertEnter",
+  --   opts = {
+  --     suggestion = {
+  --       auto_trigger = true,
+  --       keymap = {
+  --         accept = "<Tab>",
+  --         next = "<C-j>",
+  --         prev = "<C-k>",
+  --       }
+  --     }
+  --   },
+  --   init = function()
+  --     vim.cmd(":Copilot disable")
+  --   end,
+  --   keys = {
+  --     {
+  --       "<leader>cps",
+  --       "<cmd>Copilot status<CR>",
+  --     },
+  --     {
+  --       "<leader>cpe",
+  --       "<cmd>Copilot enable<CR>",
+  --     },
+  --     {
+  --       "<leader>cpd",
+  --       "<cmd>Copilot disable<CR>",
+  --     },
+  --   },
+  --
+  -- },
 
   {
     'nvim-lualine/lualine.nvim',
