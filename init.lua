e5e88556a60b4b317241f45a65f36cf83d854b0c
Theodor <theodju@gmail.com>
2025-03-18 11:09:47 +0100

    theme

--- init.lua
+++ init.lua
@@ -786,13 +786,23 @@ require('lazy').setup({
   },
 
   {
-    'sainnhe/gruvbox-material',
-    lazy = false,
+    -- 'sainnhe/gruvbox-material',
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --   vim.g.gruvbox_material_enable_italic = true
+    --   vim.cmd.colorscheme('gruvbox-material')
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_enable_italic = true
-      vim.cmd.colorscheme('gruvbox-material')
-    end
+    opts = {
+      term_colors = true,
+      transparent_background = true,
+    },
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      vim.cmd.colorscheme 'catppuccin-macchiato'
+    end,
   },
 
   -- Highlight todo, notes, etc in comments
