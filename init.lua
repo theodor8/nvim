9aecf64cd30db98850f613d8188fc8eed87c9453
Theodor <theodju@gmail.com>
2025-03-22 12:44:23 +0100

    copilot suggestion

--- init.lua
+++ init.lua
@@ -777,6 +777,7 @@ require('lazy').setup({
             -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
             group_index = 0,
           },
+          { name = "copilot", group_index = 2 },
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -22,40 +22,46 @@ return {
   --     },
   --   },
   -- },
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   cmd = "Copilot",
-  --   build = ":Copilot auth",
-  --   event = "InsertEnter",
-  --   opts = {
-  --     suggestion = {
-  --       auto_trigger = true,
-  --       keymap = {
-  --         accept = "<Tab>",
-  --         next = "<C-j>",
-  --         prev = "<C-k>",
-  --       }
-  --     }
-  --   },
-  --   init = function()
-  --     vim.cmd(":Copilot disable")
-  --   end,
-  --   keys = {
-  --     {
-  --       "<leader>cps",
-  --       "<cmd>Copilot status<CR>",
-  --     },
-  --     {
-  --       "<leader>cpe",
-  --       "<cmd>Copilot enable<CR>",
-  --     },
-  --     {
-  --       "<leader>cpd",
-  --       "<cmd>Copilot disable<CR>",
-  --     },
-  --   },
-  --
-  -- },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+        enabled = false,
+        keymap = {
+          accept = "<Tab>",
+          next = "<C-j>",
+          prev = "<C-k>",
+        }
+      }
+    },
+    init = function()
+      vim.cmd(":Copilot disable")
+    end,
+    keys = {
+      {
+        "<leader>cps",
+        "<cmd>Copilot status<CR>",
+      },
+      {
+        "<leader>cpe",
+        "<cmd>Copilot enable<CR>",
+      },
+      {
+        "<leader>cpd",
+        "<cmd>Copilot disable<CR>",
+      },
+    },
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
 
   {
     'nvim-lualine/lualine.nvim',
