5b387f3d052ffa9000bf06feb058c65ed1a6b7ee
Theodor Djurström <theodju@gmail.com>
2025-03-27 21:10:51 +0100

    H

--- init.lua
+++ init.lua
@@ -488,7 +488,7 @@ require('lazy').setup({
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
-          map('<leader>h', vim.lsp.buf.hover, '[H]over Documentation')
+          map('H', vim.lsp.buf.hover, '[H]over Documentation')
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
