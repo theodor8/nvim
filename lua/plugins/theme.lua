95a4d2db72b5a760a4dcf60ca50da3cf263abb6d
Theodor <theodju@gmail.com>
2025-04-21 16:17:00 +0200

    theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,19 +1,31 @@
 return {
-  -- 'sainnhe/gruvbox-material',
-  -- lazy = false,
-  -- priority = 1000,
-  -- config = function()
-  --   vim.g.gruvbox_material_enable_italic = true
-  --   vim.cmd.colorscheme('gruvbox-material')
-  'catppuccin/nvim',
-  name = 'catppuccin',
+  'ellisonleao/gruvbox.nvim',
   priority = 1000,
-  opts = {
-    term_colors = true,
-    transparent_background = true,
-  },
-  config = function(_, opts)
-    require('catppuccin').setup(opts)
-    vim.cmd.colorscheme 'catppuccin-macchiato'
+  config = function()
+    require('gruvbox').setup {
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = '', -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = true,
+    }
+    vim.cmd.colorscheme 'gruvbox'
   end,
 }
