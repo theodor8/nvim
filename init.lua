96a70510d4297edd18667d1d770cfea2fdb8a93b
Theodor <theodju@gmail.com>
2025-05-05 15:08:23 +0200

    vue ts

--- init.lua
+++ init.lua
@@ -477,7 +477,19 @@ require('lazy').setup({
         gopls = {},
         -- pyright = {},
         rust_analyzer = {},
-
+        volar = {},
+        ts_ls = {
+          filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+          init_options = {
+            plugins = {
+              {
+                name = '@vue/typescript-plugin',
+                location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+                languages = { 'vue' },
+              },
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
